529515c3f1bcab7420b1dbab8f682a973f2b586e
Vedant Budhabaware <vedant@example.com>
2026-01-08 22:54:55 +0530

    Fix the NoPage error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./App.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-import NoPage from './pages/Nopage.jsx';
+import NoPage from './pages/NoPage.jsx';
 
 
 const App = () => {
